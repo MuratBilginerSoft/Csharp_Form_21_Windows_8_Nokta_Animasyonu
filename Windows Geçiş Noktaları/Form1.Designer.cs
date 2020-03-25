57e66917c24b6260c9154812e077e0cd74f1ee08
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:10:11 +0300

    Initial commit

---
+++ Windows Geçiş Noktaları/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Windows_Geçiş_Noktaları
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(10, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(45, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ".";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(80, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ".";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(115, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ".";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(150, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 39);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ".";
+            this.label5.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 131);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(986, 10);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(985, 141);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows 10 Nokta Animasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Windows Geçiş Noktaları/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Geçiş_Noktaları
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x++;
+
+            if (progressBar1.Value!=29)
+            {
+                progressBar1.Value=progressBar1.Value+1;
+            }
+
+
+
+            if (x < 6)
+            {
+                switch (x)
+                {
+                    case 1: { label1.Visible = true; break; }
+
+                    case 2: { label2.Visible = true; break; }
+
+                    case 3: { label3.Visible = true; break; }
+
+                    case 4: { label4.Visible = true; break; }
+
+                    case 5: { label5.Visible = true; break; }
+
+
+
+                }
+
+            }
+
+            else if (x<16)
+            {
+                timer1.Interval = 50;
+
+                label5.Location = new Point(label5.Location.X + 35, 45);
+                label4.Location = new Point(label4.Location.X + 35, 45);
+                label3.Location = new Point(label3.Location.X + 35, 45);
+                label2.Location = new Point(label2.Location.X + 35, 45);
+                label1.Location = new Point(label1.Location.X + 35, 45);
+
+            }
+
+           
+
+            else if (x<29)
+            {
+                label5.Location = new Point(label5.Location.X + 35, 45);
+                label4.Location = new Point(label4.Location.X + 35, 45);
+                label3.Location = new Point(label3.Location.X + 35, 45);
+                label2.Location = new Point(label2.Location.X + 35, 45);
+                label1.Location = new Point(label1.Location.X + 35, 45);
+            }
+
+            else if (x<35)
+            {
+                timer1.Interval = 250;
+                 switch (x)
+                {
+                    case 30: { label1.Visible = false; break; }
+
+                    case 31: { label2.Visible = false; break; }
+
+                    case 32: { label3.Visible = false; break; }
+
+                    case 33: { label4.Visible = false; break; }
+
+                    case 34: 
+                        
+                        { 
+                            label5.Visible = false; 
+                            
+                            x = 0;
+
+                            label1.Location = new Point(10, 45);
+                            label2.Location = new Point(45, 45);
+                            label3.Location = new Point(80, 45);
+                            label4.Location = new Point(115, 45);
+                            label5.Location = new Point(140, 45);
+                            progressBar1.Value = 0;
+                            
+                            break; 
+
+
+                        }
+
+
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            progressBar1.Maximum = 29;
+           
+
+        }
+    }
+}
